5eff57e97a1954b2092a983a81124addbacdf7b1
Charisse Marie Napenas <cnapenas@admins-MacBook-Air.local>
2016-08-25 22:53:26 +0800

    added SeizureData SignatureData and initial draft for TalliesData

--- BleData/Trio/StepsData/SeizureData.cs
+++ BleData/Trio/StepsData/SeizureData.cs
@@ -7,26 +7,27 @@ using Motion.Core.Data.BleData.Trio;
 
 namespace Motion.Core.Data.BleData.Trio.StepsData
 {
-	public class SignatureData
+	public class SeizureData
 	{
-
 		const int COMMAND_PREFIX = 0x1B;
 		const int COMMAND_ID_READ = 0x26;
-		const int COMMAND_SIZE_READ = 5;
+		const int COMMAND_SIZE_READ = 4;
 
 		const int INDEX_ZERO = 0;
-		const int SIG_LEN = 3;
+		const int SEIZ_LEN = 3;
 
-		const int YEAR_DATA_LOC = 3;
-		const int MONTH_DATA_LOC = 4;
-		const int DAY_DATA_LOC = 5;
-		const int HOUR_DATA_LOC = 6;
-		const int MIN_DATA_LOC = 7;
-		const int FREQUENCY_SAMPLING_LOC = 8;
-		const int FREQUENCY_SAMPLING_TIME_LOC = 9;
-		const int SIGNATURE_PROFILE_DATA_LOC_OLD = 9;
-		const int SIGNATURE_PROFILE_DATA_LOC = 10;
+		const int BLOCK_NUMBER_LOC = 3;
+		const int YEAR_DATA_LOC = 4;
+		const int MONTH_DATA_LOC = 5;
+		const int DAY_DATA_LOC = 6;
+		const int HOUR_DATA_LOC = 7;
+		const int MIN_DATA_LOC = 8;
+		const int FREQUENCY_SAMPLING_LOC = 9;
+		const int FREQUENCY_SAMPLING_TIME_LOC = 10;
+		const int SEIZURE_PROFILE_DATA_LOC_OLD = 10;
+		const int SEIZURE_PROFILE_DATA_LOC = 11;
 
+		const int BLOCK_NUM_BYTE_SIZE = 1;
 		const int YEAR_DATA_BYTE_SIZE = 1;
 		const int MONTH_DATA_BYTE_SIZE = 1;
 		const int DAY_DATA_BYTE_SIZE = 1;
@@ -40,6 +41,9 @@ namespace Motion.Core.Data.BleData.Trio.StepsData
 		public List<byte> X_Axis;
 		public List<byte> Y_Axis;
 		public List<byte> Z_Axis;
+
+		public int BlockNumber { get; set; }
+		public int SeizureRecToRead { get; set; }
 		public int SDYear { get; set; }
 		public int SDMonth { get; set; }
 		public int SDDay { get; set; }
@@ -53,6 +57,7 @@ namespace Motion.Core.Data.BleData.Trio.StepsData
 
 		/* #### Equavalent RAW data per field #####*/
 
+		byte[] blockNumRaw;
 		byte[] yrDataRaw;
 		byte[] monthDataRaw;
 		byte[] dayDataRaw;
@@ -60,7 +65,7 @@ namespace Motion.Core.Data.BleData.Trio.StepsData
 		byte[] minDataRaw;
 		byte[] frequencySamplingRaw;
 		byte[] frequencySamplingTimeRaw;
-		byte[] signatureRaw;
+		byte[] seizureRaw;
 		byte[] writeCommandResponseCodeRaw;
 		/* ### End Raw data per field ### */
 
@@ -70,7 +75,7 @@ namespace Motion.Core.Data.BleData.Trio.StepsData
 
 		TrioDeviceInformation trioDevInfo;
 
-		public SignatureData(TrioDeviceInformation devInfo)
+		public SeizureData(TrioDeviceInformation devInfo)
 		{
 			this.trioDevInfo = devInfo;
 			this.ClearData();
@@ -82,6 +87,8 @@ namespace Motion.Core.Data.BleData.Trio.StepsData
 				Array.Clear(this._rawData, INDEX_ZERO, this._rawData.Length);
 			if (this._readCommandRawData != null && this._readCommandRawData.Length > 0)
 				Array.Clear(this._readCommandRawData, INDEX_ZERO, this._readCommandRawData.Length);
+			if (this.blockNumRaw != null && this.blockNumRaw.Length > 0)
+				Array.Clear(this.blockNumRaw, INDEX_ZERO, this.blockNumRaw.Length);
 			if (this.yrDataRaw != null && this.yrDataRaw.Length > 0)
 				Array.Clear(this.yrDataRaw, INDEX_ZERO, this.yrDataRaw.Length);
 			if (this.monthDataRaw != null && this.monthDataRaw.Length > 0)
@@ -96,8 +103,8 @@ namespace Motion.Core.Data.BleData.Trio.StepsData
 				Array.Clear(this.frequencySamplingRaw, INDEX_ZERO, this.frequencySamplingRaw.Length);
 			if (this.frequencySamplingTimeRaw != null && this.frequencySamplingTimeRaw.Length > 0)
 				Array.Clear(this.frequencySamplingTimeRaw, INDEX_ZERO, this.frequencySamplingTimeRaw.Length);
-			if (this.signatureRaw != null && this.signatureRaw.Length > 0)
-				Array.Clear(this.signatureRaw, INDEX_ZERO, this.signatureRaw.Length);
+			if (this.seizureRaw != null && this.seizureRaw.Length > 0)
+				Array.Clear(this.seizureRaw, INDEX_ZERO, this.seizureRaw.Length);
 			if (this.writeCommandResponseCodeRaw != null && this.writeCommandResponseCodeRaw.Length > 0)
 				Array.Clear(this.writeCommandResponseCodeRaw, INDEX_ZERO, this.writeCommandResponseCodeRaw.Length);
 		}
@@ -111,6 +118,7 @@ namespace Motion.Core.Data.BleData.Trio.StepsData
 				Array.Copy(rawData, this._rawData, rawData.Length);
 				this.IsReadCommand = true;
 
+				this.blockNumRaw = new byte[BLOCK_NUM_BYTE_SIZE];
 				this.yrDataRaw = new byte[YEAR_DATA_BYTE_SIZE];
 				this.monthDataRaw = new byte[MONTH_DATA_BYTE_SIZE];
 				this.dayDataRaw = new byte[DAY_DATA_BYTE_SIZE];
@@ -118,7 +126,7 @@ namespace Motion.Core.Data.BleData.Trio.StepsData
 				this.minDataRaw = new byte[MIN_DATA_BYTE_SIZE];
 				this.frequencySamplingRaw = new byte[FREQUENCY_SAMPLING_BYTE_SIZE];
 
-
+				Array.Copy(this._rawData, BLOCK_NUMBER_LOC, this.blockNumRaw, INDEX_ZERO, BLOCK_NUM_BYTE_SIZE);
 				Array.Copy(this._rawData, YEAR_DATA_LOC, this.yrDataRaw, INDEX_ZERO, YEAR_DATA_BYTE_SIZE);
 				Array.Copy(this._rawData, MONTH_DATA_LOC, this.monthDataRaw, INDEX_ZERO, MONTH_DATA_BYTE_SIZE);
 				Array.Copy(this._rawData, DAY_DATA_LOC, this.dayDataRaw, INDEX_ZERO, DAY_DATA_BYTE_SIZE);
@@ -126,6 +134,7 @@ namespace Motion.Core.Data.BleData.Trio.StepsData
 				Array.Copy(this._rawData, MIN_DATA_LOC, this.minDataRaw, INDEX_ZERO, MIN_DATA_BYTE_SIZE);
 				Array.Copy(this._rawData, FREQUENCY_SAMPLING_LOC, this.frequencySamplingRaw, INDEX_ZERO, FREQUENCY_SAMPLING_BYTE_SIZE);
 
+				this.BlockNumber = Convert.ToInt32(Utils.getDecimalValue(this.blockNumRaw));
 				this.SDYear = Convert.ToInt32(Utils.getDecimalValue(this.yrDataRaw));
 				this.SDMonth = Convert.ToInt32(Utils.getDecimalValue(this.monthDataRaw));
 				this.SDDay = Convert.ToInt32(Utils.getDecimalValue(this.dayDataRaw));
@@ -133,38 +142,44 @@ namespace Motion.Core.Data.BleData.Trio.StepsData
 				this.SDMin = Convert.ToInt32(Utils.getDecimalValue(this.minDataRaw));
 				this.FrequencySampling = Convert.ToInt32(Utils.getDecimalValue(this.frequencySamplingRaw));
 
-				if (this.trioDevInfo.ModelNumber == 961)
+				/*if (this.trioDevInfo.ModelNumber == 961)
 				{
 
 					this.frequencySamplingTimeRaw = new byte[FREQUENCY_SAMPLING_TIME_BYTE_SIZE];
 					Array.Copy(this._rawData, FREQUENCY_SAMPLING_TIME_LOC, this.frequencySamplingTimeRaw, INDEX_ZERO, FREQUENCY_SAMPLING_BYTE_SIZE);
 					this.FrequencySamplingTime = Convert.ToInt32(Utils.getDecimalValue(this.frequencySamplingTimeRaw));
-				}
+				}*/
 
 
-				int startIndexForSigData = (this.trioDevInfo.ModelNumber == 961) ? SIGNATURE_PROFILE_DATA_LOC : SIGNATURE_PROFILE_DATA_LOC_OLD;
-				int dataLen = rawData.Length - startIndexForSigData - CHECKSUM_BYTE_SIZE;
-				int sigLen = SIG_LEN;
+				int startIndexForSeizData = 9;
 
-				Array.Copy(this._rawData, startIndexForSigData, this.signatureRaw, INDEX_ZERO, dataLen);
+				if (this.trioDevInfo.ModelNumber == 961)
+					startIndexForSeizData = SEIZURE_PROFILE_DATA_LOC;
+				else if (this.trioDevInfo.ModelNumber == 939)
+					startIndexForSeizData = SEIZURE_PROFILE_DATA_LOC_OLD;
+				
+				int dataLen = rawData.Length - startIndexForSeizData - CHECKSUM_BYTE_SIZE;
+				int seizLen = SEIZ_LEN;
 
-				int signatureCount = this.signatureRaw.Length / sigLen;
+				Array.Copy(this._rawData, startIndexForSeizData, this.seizureRaw, INDEX_ZERO, dataLen);
+
+				int signatureCount = this.seizureRaw.Length / seizLen;
 
 				this.X_Axis = new List<byte>();
 				this.Y_Axis = new List<byte>();
 				this.Z_Axis = new List<byte>();
 
-				for (int i = 0; i < this.signatureRaw.Length;)
+				for (int i = 0; i < this.seizureRaw.Length;)
 				{
-					this.X_Axis.Add(this.signatureRaw[i]);
-					this.Y_Axis.Add(this.signatureRaw[i + 1]);
-					this.Z_Axis.Add(this.signatureRaw[i + 2]);
-					i += sigLen;
+					this.X_Axis.Add(this.seizureRaw[i]);
+					this.Y_Axis.Add(this.seizureRaw[i + 1]);
+					this.Z_Axis.Add(this.seizureRaw[i + 2]);
+					i += seizLen;
 
-					if (i + sigLen > this.signatureRaw.Length)
+					if (i + seizLen > this.seizureRaw.Length)
 						break;
 				}
-					
+
 			});
 
 			return parsingStatus;
@@ -176,13 +191,13 @@ namespace Motion.Core.Data.BleData.Trio.StepsData
 			{
 				this._readCommandRawData = new byte[COMMAND_SIZE_READ];
 
-				byte[] yearParam = BitConverter.GetBytes(this.SDYear);
-				byte[] monthParam = BitConverter.GetBytes(this.SDMonth);
-				byte[] dayParam = BitConverter.GetBytes(this.SDDay);
+				byte[] blockNumberParam = BitConverter.GetBytes(this.BlockNumber);
+				byte[] seizureRecToReadParam = BitConverter.GetBytes(this.SeizureRecToRead);
 
-				Buffer.BlockCopy(yearParam, INDEX_ZERO, this._readCommandRawData,2 , 1);
-				Buffer.BlockCopy(monthParam, INDEX_ZERO, this._readCommandRawData, 3, 1);
-				Buffer.BlockCopy(dayParam, INDEX_ZERO, this._readCommandRawData, 4, 1);
+
+				Buffer.BlockCopy(blockNumberParam, INDEX_ZERO, this._readCommandRawData, 2, 1);
+				Buffer.BlockCopy(seizureRecToReadParam, INDEX_ZERO, this._readCommandRawData, 3, 1);
+
 
 				byte[] commandPrefix = BitConverter.GetBytes(COMMAND_PREFIX);
 				byte[] commandID = BitConverter.GetBytes(COMMAND_ID_READ);
---
+++ BleData/Trio/StepsData/TalliesData.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Motion.Mobile.Utilities;
+using Motion.Core.Data.BleData.Trio;
+
+namespace Motion.Core.Data.BleData.Trio.StepsData
+{
+	public class TalliesData
+	{
+
+		const int COMMAND_PREFIX = 0x1B;
+		const int COMMAND_ID_READ = 0x5D;
+
+
+		const int HOURS_USED_LOC = 3;
+		const int BEACONS_CONNECTED_LOC = 5;
+		const int BEACONS_FAILED_LOC = 8;
+		const int ON_DEMAND_CONNECTED_LOC = 11;
+		const int ON_DEMAND_FAILED_LOC = 13;
+		const int NUMBER_OF_CHARGE_LOC = 15;
+		const int HARD_RESET_LOC = 17;
+		const int SCREEN_ON_DURATION_LOC = 19;
+		const int SEIZ_OR_PROF_RECORDING_COMPLETED_LOC = 23;
+		const int SEIZ_OR_PROF_RECORDING_ABORTED_LOC = 25;
+		const int NUM_OF_ALARMS_VIBRATIONS_LOC = 27;
+
+
+		const int HOURS_USED_BYTE_SIZE = 2;
+		const int BEACONS_CONNECTED_BYTE_SIZE = 3;
+		const int BEACONS_FAILED_BYTE_SIZE = 3;
+		const int ON_DEMAND_CONNECTED_BYTE_SIZE = 2;
+		const int ON_DEMAND_FAILED_BYTE_SIZE = 2;
+		const int NUMBER_OF_CHARGE_BYTE_SIZE = 2;
+		const int HARD_RESET_BYTE_SIZE = 2;
+		const int SCREEN_ON_DURATION_BYTE_SIZE = 4;
+		const int SEIZ_OR_PROF_RECORDING_COMPLETED_BYTE_SIZE = 2;
+		const int SEIZ_OR_PROF_RECORDING_ABORTED_BYTE_SIZE = 2;
+		const int NUM_OF_ALARMS_VIBRATIONS_BYTE_SIZE = 2;
+
+		public int HoursUsed { get; set; }
+		public int NumberOfBeaconsConnected { get; set; }
+		public int NumberOfBeaconsFailed { get; set; }
+		public int NumberOfOnDemandConnected { get; set; }
+		public int NumberOfOnDemandFailed { get; set; }
+		public int NumberOfCharge { get; set; }
+		public int NumberOfHardReset { get; set; }
+		public int ScreenOnDuration { get; set; }
+		public int SeizureOrProfileRecordingCompleted { get; set; }
+		public int SeizureOrProfileRecordingAborted { get; set; }
+		public int NumberOfAlarmsOrVibrations { get; set; }
+
+		/* #### Equavalent RAW data per field #####*/
+
+		byte[] hoursUsedRaw;
+		byte[] beaconsConnectedRaw;
+		byte[] beaconsFailedRaw;
+		byte[] onDemandConnectedRaw;
+		byte[] onDemandFailedRaw;
+		byte[] numberOfChargeRaw;
+		byte[] numberOfHardResetRaw;
+		byte[] screenOnDurationRaw;
+		byte[] seizOrProfRecordingCompletedRaw;
+		byte[] seizOrProfRecordingAbortedRaw;
+		byte[] numOfAlarmsVibrationsRaw;
+		byte[] writeCommandResponseCodeRaw;
+		/* ### End Raw data per field ### */
+
+
+		public byte[] _rawData { get; set; }
+		public byte[] _readCommandRawData { get; set; }
+
+		TrioDeviceInformation trioDevInfo;
+
+		public TalliesData(TrioDeviceInformation devInfo)
+		{
+			this.trioDevInfo = devInfo;
+			this.ClearData();
+		}
+
+		private void ClearData()
+		{
+			
+		}
+	}
+}
+
